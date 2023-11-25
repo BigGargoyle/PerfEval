38337ad3a2dcb6ef81a86441940df33fbfa22340
Dominik Hrdý <dominik.hrdy@email.cz>
2023-11-25 16:56:55 +0100

    úprava a dodání testů

--- implementation/PerfEval/src/test/java/HierarchicalBootstrapTest.java
+++ implementation/PerfEval/src/test/java/HierarchicalBootstrapTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class HierarchicalBootstrapTest {
 
     @Test
-    public void testEvaluateBootstrap() {
+    public void testEvaluateBootstrapDifferentSets() {
         HierarchicalBootstrap bootstrap = new HierarchicalBootstrap(0.05, 10_000);
 
         // Test evaluateBootstrap with sampleSet1 and sampleSet2
@@ -16,6 +16,10 @@ public class HierarchicalBootstrapTest {
         double[][] sampleSet2 = {{7.0, 8.0, 9.0}, {10.0, 11.0, 12.0}};
         boolean result = bootstrap.evaluateBootstrap(sampleSet1, sampleSet2);
         assertFalse(result);
+    }
+    @Test
+    public void testEvaluateBootstrapSimilarSets() {
+        HierarchicalBootstrap bootstrap = new HierarchicalBootstrap(0.05, 10_000);
 
         // Test evaluateBootstrap with different data that should not satisfy the condition
         double[][] sampleSet3 = {{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}};
@@ -23,6 +27,49 @@ public class HierarchicalBootstrapTest {
         boolean result2 = bootstrap.evaluateBootstrap(sampleSet3, sampleSet4);
         assertTrue(result2);
     }
+
+    @Test
+    public void testEvaluateBootstrapDifferentSetsLargeArrays() {
+        HierarchicalBootstrap bootstrap = new HierarchicalBootstrap(0.05, 10_000);
+
+        // Test evaluateBootstrap with larger sampleSet1 and sampleSet2
+        int size = 100; // Adjust size as needed
+        double[][] sampleSet1 = new double[size][size];
+        double[][] sampleSet2 = new double[size][size];
+
+        // Populate sampleSet1 and sampleSet2 with significantly different values
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                sampleSet1[i][j] = i + j; // Some different values for sampleSet1
+                sampleSet2[i][j] = i + j + 10; // Values shifted by 10 for sampleSet2
+            }
+        }
+
+        boolean result = bootstrap.evaluateBootstrap(sampleSet1, sampleSet2);
+        assertFalse(result);
+    }
+    @Test
+    public void testEvaluateBootstrapSimilarSetsLargeArrays() {
+        HierarchicalBootstrap bootstrap = new HierarchicalBootstrap(0.05, 10_000);
+
+        // Test evaluateBootstrap with larger sampleSet3 and sampleSet4
+        int size = 100; // Adjust size as needed
+        double[][] sampleSet3 = new double[size][size];
+        double[][] sampleSet4 = new double[size][size];
+
+        // Populate sampleSet3 and sampleSet4 with similar values
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                sampleSet3[i][j] = i + j; // Some values for sampleSet3
+                sampleSet4[i][j] = i + j; // Same values for sampleSet4 as sampleSet3
+            }
+        }
+
+        boolean result2 = bootstrap.evaluateBootstrap(sampleSet3, sampleSet4);
+        assertTrue(result2);
+    }
+
+
 
     @Test
     public void testCalcCIInterval() {
--- implementation/PerfEval/src/test/java/StatisticTTestTest.java
+++ implementation/PerfEval/src/test/java/StatisticTTestTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StatisticTTestTest {
 
     @Test
-    public void testAreSetsDifferent() {
+    public void testDifferentSets() {
         StatisticTTest tTest = new StatisticTTest(0.05);
 
         // Test when sampleSet1 and sampleSet2 are significantly different
@@ -15,21 +15,67 @@ public class StatisticTTestTest {
         double[][] sampleSet2 = {{7.0, 8.0, 9.0}, {10.0, 11.0, 12.0}};
         boolean result = tTest.areSetsSame(sampleSet1, sampleSet2);
         assertFalse(result);
+    }
+    @Test
+    public void testSimilarSets() {
+        StatisticTTest tTest = new StatisticTTest(0.05);
 
         // Test when sampleSet1 and sampleSet2 are not significantly different
         double[][] sampleSet3 = {{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}};
         double[][] sampleSet4 = {{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}};
         boolean result2 = tTest.areSetsSame(sampleSet3, sampleSet4);
         assertTrue(result2);
+    }
+    @Test
+    public void testEmptySets() {
+        StatisticTTest tTest = new StatisticTTest(0.05);
 
         // Test when the sample sets are empty, which should throw a MathIllegalArgumentException
         double[][] emptySampleSet1 = {};
         double[][] emptySampleSet2 = {};
-        try {
-            tTest.areSetsSame(emptySampleSet1, emptySampleSet2);
-            fail("MathIllegalArgumentException should be thrown");
-        } catch (MathIllegalArgumentException e) {
-            // This exception is expected
-        }
+
+        assertThrows(MathIllegalArgumentException.class, () -> tTest.areSetsSame(emptySampleSet1, emptySampleSet2));
     }
+    @Test
+    public void testDifferentSetsLargeCount() {
+        StatisticTTest tTest = new StatisticTTest(0.05);
+
+        // Test when sampleSet1 and sampleSet2 are significantly different with a large count of elements
+        double[][] sampleSet1 = new double[2][1000];
+        double[][] sampleSet2 = new double[2][1000];
+
+        // Populate sampleSet1 and sampleSet2 with different values
+        for (int i = 0; i < 1000; i++) {
+            sampleSet1[0][i] = i + 1;  // Values from 1 to 1000
+            sampleSet1[1][i] = i + 1001;  // Values from 1001 to 2000
+
+            sampleSet2[0][i] = i + 2001;  // Values from 2001 to 3000
+            sampleSet2[1][i] = i + 3001;  // Values from 3001 to 4000
+        }
+
+        boolean result = tTest.areSetsSame(sampleSet1, sampleSet2);
+        assertFalse(result);
+    }
+
+    @Test
+    public void testSimilarSetsLargeCount() {
+        StatisticTTest tTest = new StatisticTTest(0.05);
+
+        // Test when sampleSet1 and sampleSet2 are not significantly different with a large count of elements
+        double[][] sampleSet3 = new double[2][1000];
+        double[][] sampleSet4 = new double[2][1000];
+
+        // Populate sampleSet3 and sampleSet4 with similar values
+        for (int i = 0; i < 1000; i++) {
+            sampleSet3[0][i] = i + 1;  // Values from 1 to 1000
+            sampleSet3[1][i] = i + 1001;  // Values from 1001 to 2000
+
+            sampleSet4[0][i] = i + 1;  // Values from 1 to 1000
+            sampleSet4[1][i] = i + 1001;  // Values from 1001 to 2000
+        }
+
+        boolean result2 = tTest.areSetsSame(sampleSet3, sampleSet4);
+        assertTrue(result2);
+    }
+
 }
